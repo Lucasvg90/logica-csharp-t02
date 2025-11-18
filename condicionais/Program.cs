d573afb5b51ceb45677b85730b5c5e1a7e8402a1
Lucas Sousa <lucastomsaw@gmail.com>
2025-11-18 17:18:25 -0300

    Add files via upload

---
+++ condicionais/Program.cs
@@ -0,0 +1,102 @@
+﻿using static System.Console;
+
+//Exemplo
+
+
+string nome = "Pablo Escobar";
+
+if (nome == "Ana")
+{
+    WriteLine("Fala, fi");
+} else if (nome == "Pablo Escobar")
+{
+    WriteLine("Epa, perai...");
+} else if(string.IsNullOrEmpty(nome))
+{
+    WriteLine("Falar o nome ninguém quer, né");
+
+} else 
+{
+    WriteLine("Mano, nem te conheço, o que você está fazendo na minha casa?");
+}
+
+//Exemplo 2
+
+WriteLine("\nE a renda, man? Tá como?");
+
+int renda = Convert.ToInt16(ReadLine());
+
+if (renda >= 1000)
+{
+    WriteLine("Tá podendo almossar");
+}
+else
+{
+    WriteLine("Não tá podeno almossar");
+}
+
+// Exemplo 3 
+WriteLine("\nMim dê numero");
+
+int numero = Convert.ToInt16(ReadLine());
+
+if(numero % 2 == 0)
+{
+    WriteLine("Pelo visto, é par");
+}
+else
+{
+    WriteLine("Ao que vejo, é ímpar");
+}
+
+
+// Exemplo 4
+WriteLine("\nQue nota?");
+double nota = Convert.ToDouble(ReadLine());
+
+if (nota > 8.5)
+{
+    WriteLine("Você é o bichão mesmo, hein doido");
+} else if (nota >= 6)
+{
+    WriteLine("Deu para passar, né");
+}
+else if(nota >= 3)
+{
+    WriteLine("Vish, não tá dando para passar, \nvocê tá de recuperação mas tem chance ainda");
+    
+} else
+{
+    WriteLine("Lascou");
+}
+
+
+// Exemplo 5
+
+double pontuacao = 9.9;
+
+int result = (int)Math.Round(pontuacao);
+
+Console.WriteLine(result);
+
+switch (result)
+{
+    case 10:
+        WriteLine("Aí sim, hein");
+        break;
+
+    case >= 7:
+        WriteLine("Tá bom");
+        break;
+
+    case >= 6:
+        WriteLine("Tá de rec, man");
+        break;
+    case >= 3:
+        WriteLine("Tá pra reprovar, pai");
+        break;
+
+    default: 
+        WriteLine("Foi reprovado, fi, receba");
+        break;
+}
---
+++ maratonaexercicio01/Program.cs
@@ -0,0 +1,115 @@
+﻿using static System.Console;
+
+/* Crie um script que declare uma variável para o nome de 
+uma pessoa e exiba uma saudação personalizada, por exemplo: "Olá, 
+João! Bem-vindo ao nosso site!"*/
+
+WriteLine("\n--- Exercicio 01 ---\n");
+
+WriteLine("Qual é o seu nome, dog?");
+
+string nome = ReadLine();
+
+WriteLine($"Olá, {nome}! Bem vindo ao nosso site! ");
+
+WriteLine("Olá, " + nome + "! Bem vindo ao nosso site!");
+
+
+
+/*: Crie um script que converta uma temperatura em Celsius 
+para Fahrenheit*/
+
+WriteLine("\n--- Exercicio 02 ---\n");
+
+WriteLine("Digite a temperatura em Celsius: ");
+
+double celsius = Convert.ToDouble(ReadLine());
+
+double fahrenheit = (celsius * (9 / 5)) + 32;
+
+WriteLine($"A temperatura em Fahrenheit é: {fahrenheit}");
+
+
+/*Crie um script que solicite ao usuário seu nome, idade e 
+cidade e depois mostre uma mensagem com essas informações. 
+Exemplo: "Meu nome é João, tenho 25 anos e moro em São Paulo".*/
+
+WriteLine("\n--- Exercicio 03 ---\n");
+
+WriteLine("Qual é o teu nome, paizao?");
+
+String nome3 = ReadLine();
+
+WriteLine("E quantos anos tem?");
+
+int idade = Convert.ToInt32(Console.ReadLine());
+
+WriteLine("E qual é a tua cidade, meu rei?");
+
+String cidade = ReadLine();
+
+WriteLine($"\nMeu nome é {nome3}\nTenho {idade} anos e moro lá em {cidade}");
+WriteLine($"\nMeu nome é " + nome3 + "\nTenho " + idade + " anos e moro lá em " + cidade);
+
+
+
+/* Crie um script que calcule o valor de uma parcela de um 
+empréstimo, dado o valor total do empréstimo, a taxa de juros e o 
+número de parcelas*/
+
+WriteLine("\n--- Exercicio 04 ---\n");
+
+WriteLine("Manda o valor total do seu empréstimo, dog");
+
+double emprestimo = Convert.ToDouble(Console.ReadLine());
+
+WriteLine("Em quantas parcelas você vai querer pagar por isso aí?");
+int parcelas = Convert.ToInt16(Console.ReadLine());
+
+WriteLine("E qual a taxa de juros?");
+double juros = Convert.ToDouble(Console.ReadLine()) / 100;
+
+double valorParcela = emprestimo * (1 + juros) / parcelas;
+
+WriteLine($"O valor a ser pago por parcela é:\n {valorParcela}");
+
+
+/* Crie um script que converta uma quantia em reais para 
+dólares, com base em uma taxa de câmbio fornecida. Mostre o valor 
+convertido com 2 casas decimais.
+*/
+
+WriteLine("\n--- Exercicio 05 ---\n");
+
+WriteLine("Informe quantos reais gostaria de converter: ");
+
+double real = Convert.ToDouble(Console.ReadLine());
+
+WriteLine("Informe a cotação atual do Dólar: ");
+
+double dolar = Convert.ToDouble(ReadLine());
+
+double conversao = real * dolar;
+
+WriteLine($"A sua conversão deu: ${Math.Round(conversao, 2)}");
+
+
+
+
+/*  Crie um script que calcule o valor do desconto de um 
+produto, dado o preço original e a porcentagem de desconto. */
+
+WriteLine("\n--- Exercicio 06 ---\n");
+
+WriteLine("\nInforme o valor do produto: ");
+
+double produto = Convert.ToDouble(ReadLine());
+
+WriteLine("\nInforme seu desconto em %: ");
+
+double desconto = (Convert.ToDouble(ReadLine()) / 100);
+
+
+double precoTotal = produto - (produto * desconto);
+
+WriteLine($"Seu preço total foi de: \nR${Math.Round(precoTotal, 2)}");
