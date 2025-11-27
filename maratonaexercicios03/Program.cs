e8d95ed994de2f688a82913ea0528d84edb804b4
lucasvg90 <lucastomsaw@gmail.com>
2025-11-27 17:00:54 -0300

    Maratona Exercicios 3

---
+++ maratonaexercicios03/Program.cs
@@ -0,0 +1,13 @@
+﻿//Ex 01
+
+string[] pecas = { "Carburador", "Roda", "Carcaça", 
+    "volante", "Aro", "peça6", "peça7", "peça8", 
+    "peça9", "peça10" };
+
+foreach(string peca in pecas)
+{
+    Console.WriteLine(peca);
+}
+
+//Ex 02
+
