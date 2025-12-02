a081552906d7cbddf7ec4becd33ceaacea044504
lucasvg90 <lucastomsaw@gmail.com>
2025-12-02 16:56:16 -0300

    tome

---
+++ classes/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace logica13
+{
+    internal class Carro
+    {
+        public string Marca { get; set; }
+
+        private string Modelo { get; set; }
+
+        private int _ano;
+
+        public int Ano {
+            get { return _ano; }
+            set
+            {
+                if(value > 1900 && value <= DateTime.Now.Year)
+                {
+                    _ano = value;
+                } else
+                {
+                    Console.WriteLine("Ano inválido");
+                }
+            }
+        }
+
+    }
+}
